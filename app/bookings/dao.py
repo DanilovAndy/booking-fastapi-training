5f41ada185d2a73d42735965b049fcccc5e45cdd
AndersGrunge <morettiandrewd@gmail.com>
2024-05-20 22:21:18 +0300

    feat: create base DAO class with trivial queries for variable models

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -1,14 +1,6 @@
-from sqlalchemy import select
-
 from app.bookings.models import Bookings
-from app.database import async_session_maker
+from app.dao.base import BaseDAO
 
 
-class BookingDAO:
-
-    @classmethod
-    async def find_all(cls):
-        async with async_session_maker() as session:
-            query = select(Bookings)
-            bookings = await session.execute(query)
-            return bookings.mappings().all()
+class BookingDAO(BaseDAO):
+    model = Bookings
