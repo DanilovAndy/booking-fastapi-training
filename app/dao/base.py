5a4fd33998ceb81d9c74090163ebbe98d4362224
AndersGrunge <morettiandrewd@gmail.com>
2024-05-21 23:38:31 +0300

    feat: add password hashing, user registration route

--- app/dao/base.py
+++ app/dao/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select
+from sqlalchemy import select, insert
 
 from app.database import async_session_maker
 
@@ -26,3 +26,10 @@ class BaseDAO:
             query = select(cls.model.__table__.columns).filter_by(**filter_by)
             result = await session.execute(query)
             return result.mappings().all()
+
+    @classmethod
+    async def add(cls, **data):
+        async with async_session_maker() as session:
+            query = insert(cls.model).values(**data)
+            await session.execute(query)
+            await session.commit()
---
+++ app/users/router.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, HTTPException
+
+from app.users.auth import get_password_hash
+from app.users.dao import UsersDAO
+from app.users.schemas import SUserRegister
+
+router = APIRouter(
+    prefix='/auth',
+    tags=["Auth & Users"]
+)
+
+
+@router.post("/register")
+async def register_user(user_data: SUserRegister):
+    existing_user = await UsersDAO.find_one_or_none(email=user_data.email)
+    if existing_user is not None:
+        raise HTTPException(status_code=403)
+    hashed_password = get_password_hash(user_data.password)
+    await UsersDAO.add(email=user_data.email, hashed_password=hashed_password)
