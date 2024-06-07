08b4fe5d891da9f4744209ef0a2dd897ce3f5fda
AndersGrunge <morettiandrewd@gmail.com>
2024-06-07 23:40:53 +0300

    feat: add route for checking free rooms

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from fastapi import APIRouter, Depends
 
 from app.bookings.dao import BookingDAO
@@ -14,3 +16,11 @@ router = APIRouter(
 @router.get("")
 async def get_bookings(user: Users = Depends(get_cur_user)) -> list[SBooking]:
     return await BookingDAO.find_all(user_id=user.id)
+
+
+@router.post("")
+async def add_booking(
+        room_id: int, date_from: date, date_to: date,
+        user: Users = Depends(get_cur_user)
+):
+    return await BookingDAO.add(user.id, room_id, date_from, date_to)
--- app/main.py
+++ app/main.py
@@ -45,14 +45,3 @@ def get_hotels(
         }
     ]
     return hotels
-
-
-class SBooking(BaseModel):
-    room_id: int
-    date_from: date
-    date_to: date
-
-
-@app.post("/bookings")
-def add_booking(booking: SBooking):
-    pass
