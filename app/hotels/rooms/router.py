fdc783272aad386c661716592bae4f1a642dc72b
AndersGrunge <morettiandrewd@gmail.com>
2024-06-14 23:50:52 +0300

    feat: add routes and dao method for getting all free rooms of the hotel in dates

---
+++ app/hotels/rooms/router.py
@@ -0,0 +1,17 @@
+from datetime import date, datetime, timedelta
+
+from fastapi import APIRouter, Query
+
+from app.hotels.rooms.dao import RoomsDAO
+
+router = APIRouter(prefix="/hotels", tags=["Rooms"])
+
+
+@router.get("/{hotel_id}/rooms")
+async def get_rooms(
+        hotel_id: int,
+        date_from: date = Query(..., description=f"Например, {datetime.now().date()}"),
+        date_to: date = Query(..., description=f"Например, {(datetime.now() + timedelta(days=14)).date()}"),
+):
+    rooms = await RoomsDAO.find_all(hotel_id, date_from, date_to)
+    return rooms
--- app/main.py
+++ app/main.py
@@ -6,12 +6,14 @@ from pydantic import BaseModel
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
+from app.hotels.rooms.router import router as router_rooms
 
 app = FastAPI()
 
 app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_hotels)
+app.include_router(router_rooms)
 
 
 class HotelsSearchArgs:
