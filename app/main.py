70cf37764cb6dbaabd9069c67b870df9fa5d7f9f
AndersGrunge <morettiandrewd@gmail.com>
2024-06-20 00:18:12 +0300

    feat: create frontend template for getting hotels by location

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
+from app.pages.router import router as pages_router
 
 app = FastAPI()
 
@@ -14,6 +15,7 @@ app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_hotels)
 app.include_router(router_rooms)
+app.include_router(pages_router)
 
 
 class HotelsSearchArgs:
