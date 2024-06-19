51ce18f944ec26f7ce1b85aff0c5133bb7bbf73f
AndersGrunge <morettiandrewd@gmail.com>
2024-06-20 00:06:02 +0300

    fix: fix exceptions to be raised

--- app/hotels/router.py
+++ app/hotels/router.py
@@ -2,6 +2,7 @@ from datetime import date, datetime, timedelta
 
 from fastapi import APIRouter, Query, status
 
+from app.exceptions import RoomCannotBeBooked
 from app.hotels.dao import HotelsDAO
 
 router = APIRouter(prefix="/hotels", tags=["Hotels"])
@@ -14,8 +15,8 @@ async def get_hotels_by_location_and_time(
         date_to: date = Query(..., description=f"Например, {(datetime.now() + timedelta(days=14)).date()}"),
 ):
     if date_from > date_to:
-        raise status.HTTP_409_CONFLICT
+        raise RoomCannotBeBooked
     if (date_to - date_from).days > 31:
-        raise status.HTTP_409_CONFLICT
+        raise RoomCannotBeBooked
     hotels = await HotelsDAO.find_all(location, date_from, date_to)
     return hotels
