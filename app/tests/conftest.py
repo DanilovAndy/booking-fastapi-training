0e967e66fc703929c58b23936cac3f253510026c
AndersGrunge <morettiandrewd@gmail.com>
2024-07-21 00:24:19 +0300

    test: prepare client and session fixtures

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -13,6 +13,9 @@ from app.hotels.rooms.models import Rooms
 from app.main import app as fastapi_app
 from app.users.models import Users
 
+from fastapi.testclient import TestClient
+from httpx import AsyncClient
+
 
 @pytest.fixture(scope="session", autouse=True)
 async def prepare_database():
@@ -55,3 +58,15 @@ def event_loop(request):
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
+
+
+@pytest.fixture(scope="function")
+async def async_client():
+    async with AsyncClient(app=fastapi_app, base_url="http://test") as ac:
+        yield ac
+
+
+@pytest.fixture(scope="function")
+async def session():
+    async with async_session_maker() as session:
+        yield session
