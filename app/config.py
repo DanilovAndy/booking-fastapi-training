8085257dafa6d2d45e80584fafd66037099cb8e9
AndersGrunge <morettiandrewd@gmail.com>
2024-07-20 23:44:39 +0300

    test: set pytest environment and DB fixture

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import os
+from typing import Literal
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -7,11 +8,19 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=os.path.join(os.path.dirname(__file__), '../.env'),
                                       env_file_encoding='utf-8', extra='ignore')
 
+    MODE: Literal["DEV", "TEST", "PROD"]
+
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
+
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
 
     SMTP_HOST: str
     SMTP_PORT: int
---
+++ app/tests/conftest.py
@@ -0,0 +1,57 @@
+import asyncio
+import json
+from datetime import datetime
+
+import pytest
+from sqlalchemy import insert
+
+from app.bookings.models import Bookings
+from app.config import settings
+from app.database import Base, async_session_maker, engine
+from app.hotels.models import Hotels
+from app.hotels.rooms.models import Rooms
+from app.main import app as fastapi_app
+from app.users.models import Users
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def prepare_database():
+    assert settings.MODE == "TEST"
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    def open_mock_json(model: str):
+        with open(f"app/test/mock_{model}.json", encoding="utf-8") as file:
+            return json.load(file)
+
+    hotels = open_mock_json("hotels")
+    rooms = open_mock_json("rooms")
+    users = open_mock_json("users")
+    bookings = open_mock_json("bookings")
+
+    for booking in bookings:
+        booking["date_from"] = datetime.strptime(booking["date_from"], "%Y-%m-%d")
+        booking["date_to"] = datetime.strptime(booking["date_to"], "%Y-%m-%d")
+
+    async with async_session_maker() as session:
+        add_hotels = insert(Hotels).values(hotels)
+        add_rooms = insert(Rooms).values(rooms)
+        add_users = insert(Users).values(users)
+        add_bookings = insert(Bookings).values(bookings)
+
+        await session.execute(add_hotels)
+        await session.execute(add_rooms)
+        await session.execute(add_users)
+        await session.execute(add_bookings)
+
+        await session.commit()
+
+
+@pytest.fixture(scope="session")
+def event_loop(request):
+    """Create an instance of the default event loop for each test case."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
