7ed633ecbe205a92b94d879f69992689b44ce635
AndersGrunge <morettiandrewd@gmail.com>
2024-08-10 23:22:58 +0300

    fix: usage of deprecated sessionmaker

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import NullPool
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 from app.config import settings
 
 if settings.MODE == "TEST":
@@ -14,7 +14,7 @@ else:
 
 engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
 
-async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
 class Base(DeclarativeBase):
