9c58fe9aa4e8f782f7f1dcabf632f139f2a47170
AndersGrunge <morettiandrewd@gmail.com>
2024-10-29 19:00:36 +0300

    feat: change mock email to real

--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -27,8 +27,7 @@ def send_booking_confirmation_email(
         booking: dict,
         email_to: EmailStr,
 ):
-    email_to_mock = settings.SMTP_USER
-    msg_content = create_booking_confirmation_template(booking, email_to_mock)
+    msg_content = create_booking_confirmation_template(booking, email_to)
 
     with smtplib.SMTP_SSL(settings.SMTP_HOST, settings.SMTP_PORT) as server:
         server.login(settings.SMTP_USER, settings.SMTP_PASS)
